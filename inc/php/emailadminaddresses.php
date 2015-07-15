1b0ec0b537893041ae9705714726143cc7ed48f0
therealaslum <yanni.b.c@gmail.com>
2015-07-15 16:34:48 -0400

    Add non-JS includeds, and ignores
    This adds in CSS files, plus inc/php/ files, but ignores all files that
    contain credentials. All of these PHP includes will need to be uploaded
    seperately and by hand.

---
+++ inc/php/emailadminaddresses.php
@@ -0,0 +1,18 @@
+<?
+$bemailadmin =array("esensabaugh@mfrl.org"=>"Elizabeth Sensabaugh",
+					"cprati@mfrl.org"=>"Cathryne Prati");
+$cemailadmin =array("phale@mfrl.org"=>"Pamela Hale",
+					"jtabor@mfrl.org"=>"Jessica Tabor",
+					"jbarrowclough@mfrl.org"=>"Jenny Barrowclough",
+					
+					"scole@mfrl.org"=>"Stacey Cole");
+$femailadmin =array("cwhitten@mfrl.org"=>"Cathryne Whitten",
+					"lkaluszka@mfrl.org"=>"Lori Kaluszka");
+$memailadmin =array("cminnick@mfrl.org"=>"Cindy Minnick",
+					"jwren@mfrl.org"=>"Janet Wren");
+$nbemailadmin=array("lspivey@mfrl.org"=>"Linda Spivey");
+$raemailadmins=array("esensabaugh@mfrl.org"=>"Elizabeth Sensabaugh",
+					"phale@mfrl.org"=>"Pamela Hale",
+					"cwhitten@mfrl.org"=>"Cathryne Whitten",
+					"cminnick@mfrl.org"=>"Cindy Minnick");
+?>
