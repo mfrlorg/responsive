03fa4d26439755006c35dfa01a2a33ae3d18fe92
therealaslum <yanni.b.c@gmail.com>
2015-07-22 10:53:42 -0400

    Inconsequential change
    Change internal copydate to 2015 to test if hooks are working.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 $whatserveristhis = 'thisisthetestserver.txt';
 if (file_exists($whatserveristhis)) {$server="test";}
 // ********
-// * 2014 *
+// * 2015 *
 // ********
 
 if($_REQUEST['slides']!='') $slides=intval($_REQUEST['slides']);
