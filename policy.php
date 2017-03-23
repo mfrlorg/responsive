3f240292cc690806b9a1a286f100903ca31c9302
therealaslum <yanni.b.c@gmail.com>
2017-03-23 16:21:11 -0400

    Revert "More Obvious typo"
    This reverts commit 7d0b90716ac0623d8e9cb7fda591b9bd4bc681e1.

--- policy.php
+++ policy.php
@@ -10,7 +10,7 @@ if (file_exists($whatserveristhis)) {$server="test";}
 	include 'inlibrary.php';?>
 <html>
 <head>
-<title>Policy</title>
+<title>Police E</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="google-site-verification" content="kwYyWkz6FQf_DQmvAwDvYR1Ccb2UmOn_tqnHVYAEBTM" />
@@ -57,7 +57,7 @@ font-size:11px;
 		</div> <!-- End Menu col -->
 		<div id="fullrightcol">
 			<div class="pagediv">
-<h1>Policies EDITED</h1>
+<h1>Policies</h1>
 
 
 
